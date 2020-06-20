5c72291f51be5b51c665e372f865e6651c0f845e
hogyun.yu <hogyun.yu@navercorp.com>
2020-06-20 17:55:17 +0900

    Updates

---
+++ precache-manifest.a83fdcafe9c242a4e49af5544bb099ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93551c28d140c509a86d3c9abf9becf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a189256d0b06870df030",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9fdc934e9fe15dd51a62",
+    "url": "/static/css/main.5338e8f9.chunk.css"
+  },
+  {
+    "revision": "a189256d0b06870df030",
+    "url": "/static/js/2.fbacc94c.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.fbacc94c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fdc934e9fe15dd51a62",
+    "url": "/static/js/main.c377e75d.chunk.js"
+  },
+  {
+    "revision": "abde2a296e2480f2290a",
+    "url": "/static/js/runtime-main.875f34af.js"
+  }
+]);
